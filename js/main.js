8e4e0263c6eb416ba156bf740ef6ce803f687cf0
emaduddin678 <emaduddin678@gmail.com>
2022-05-28 01:33:17 +0600

    js start

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 const milestonesData = JSON.parse(data).data;
 
+//load course milestones data
+function loadMilestones(){
+    const milestones = document.querySelector('.milestones');
+
+    milestones.innerHTML = `<div class="milestone border-b">
+    <div class="flex">
+        <div class="checkbox"><input type="checkbox"></div>
+        <div>
+            <p>
+                Milestone 1 name
+                <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+        </div>
+    </div>
+    <div class="hidden_panel1">
+        <div class="module border-b">
+            <p>Module name</p>
+        </div>
+    </div>
+</div>`;
+    
+}
