ed389b520fafd81bd16223e71aefd74af81b8f19
emaduddin678 <emaduddin678@gmail.com>
2022-06-02 00:40:34 +0600

    almost done

--- js/main.js
+++ js/main.js
@@ -6,25 +6,60 @@ function loadMilestones(){
 
     milestones.innerHTML = `${milestonesData.map(function(milestone){
         return `<div class="milestone border-b">
-        <div class="flex">
-            <div class="checkbox"><input type="checkbox"></div>
-            <div>
-                <p>
-                    ${milestone.name}
-                    <span><i class="fas fa-chevron-down"></i></span>
-                </p>
-            </div>
-        </div>
-        <div class="hidden_panel">
-            ${milestone.modules.map(function(module){
-                return `<div class="module border-b">
-                            <p>${module.name}</p>
-                        </div>`;
-            }).join("")}
-        </div>
-    </div>`;
+                    <div class="flex">
+                        <div class="checkbox"><input type="checkbox"></div>
+                        <div onclick = "openMilestone(this, ${milestone._id})">
+                            <p>
+                                ${milestone.name}
+                                <span><i class="fas fa-chevron-down"></i></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="hidden_panel ">
+                        ${milestone.modules.map(function(module){
+                            return `<div class="module border-b">
+                                        <p>${module.name}</p>
+                                    </div>`;
+                        }).join("")}
+                    </div>
+                </div>`;
     }).join("")}`;
     
+}
+
+
+function openMilestone(milestoneElement, id){
+    const currentPanel = milestoneElement.parentNode.nextElementSibling;
+
+    const shownPanel = document.querySelector(".show");
+    if(!currentPanel.classList.contains("show") && shownPanel){
+        shownPanel.classList.remove("show");
+    }
+    currentPanel.classList.toggle("show");
+
+
+    const activePanel = document.querySelector(".active");
+    if(activePanel && !milestoneElement.classList.contains("active")){
+        activePanel.classList.remove("active");
+    }
+    milestoneElement.classList.toggle("active");
+
+    showMilestones(id);
+    
+}
+ 
+
+function showMilestones(id){
+    const milestoneImage  = document.querySelector(".milestoneImage");
+    const name  = document.querySelector(".title");
+    const details  = document.querySelector(".details");
+
+    milestoneImage.src = milestonesData[id].image;
+    name.innerText = milestonesData[id].name;
+    details.innerText = milestonesData[id].description;
+
+
+
 }
 
 loadMilestones()
